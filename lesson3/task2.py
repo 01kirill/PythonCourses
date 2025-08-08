f52f3c0eb73672cc0de40405c07fe0114702c6f5
ksqucwkkz <ksqu.wkkz@gmail.com>
2025-08-08 13:47:10 +0300

    lesson tasks

---
+++ lesson3/task2.py
@@ -0,0 +1,14 @@
+int_value = 5
+print(id(int_value))
+int_value += 1
+print(id(int_value))
+
+list = [1, 2, 3]
+print(id(list))
+list.append(4)
+print(id(list))
+
+float_value = 5.5
+print(id(float_value))
+float_value += 1
+print(id(float_value))
